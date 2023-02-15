4f7ea8a5e440386a303db28702009a61b7e509b0
larissaRF <larissa_lrf@hotmail.com>
2023-02-14 21:48:06 -0300

    foi

--- Node Js/Pasta A/Pasta B/AcesarModulo.js
+++ Node Js/Pasta A/Pasta B/AcesarModulo.js
@@ -4,8 +4,8 @@ console.log(moduloA.ola)
 const saudacao = require('saudacao')
 console.log(saudacao.ola)
 
-const http = require('http')
-http.createServer((req, res) => {
-    res.write('Bom dia!')
-    res.end()
-}).listen(8080)
+//const http = require('http')
+//http.createServer((req, res) => {
+   // res.write('Bom dia!')
+    //res.end()
+//}).listen(8080)
