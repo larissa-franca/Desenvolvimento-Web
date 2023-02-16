b1efedb8777547606f30ab827a2e49aee55685bf
larissaRF <larissa_lrf@hotmail.com>
2023-02-16 20:18:07 -0300

    exc

--- Node Js/exportar.js
+++ Node Js/exportar.js
@@ -1,2 +1,14 @@
 console.log(module.exports === this)
-console.log(module.exports === this)
+console.log(module.exports === exports)
+
+this.a = 1
+exports.b = 2
+module.exports.c = 3
+
+exports = null
+console.log(module.exports)
+
+//continua apontando para o 1, 2, 3 por que exports é uma variável que aonta para outro objeto,
+//entao tanto faz como aponta, no final quem será apontado é o que consta dentro de "module.exports"
+
+
