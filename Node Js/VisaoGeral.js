a58686542fe9521a694f2bdfed8c91e5b59ba27e
larissaRF <larissa_lrf@hotmail.com>
2023-02-15 21:57:59 -0300

    ;

--- Node Js/VisaoGeral.js
+++ Node Js/VisaoGeral.js
@@ -1,7 +1,7 @@
-//HTML é gerado, mas só é rodado no browser
-//o servidor prove as APIs
-//o backend roda em varias linuagens, já o frontend roda no browser
-//JS pode ser trabalhado dentro do browser, como frontend
+//HTML é gerado, mas só é rodado no browser;
+//o servidor prove as APIs;
+//o backend roda em varias linuagens, já o frontend roda no browser;
+//JS pode ser trabalhado dentro do browser, como frontend;
 //leyers - padrão de camadas
 //dentro do node um arquivo representa um módulo
 
