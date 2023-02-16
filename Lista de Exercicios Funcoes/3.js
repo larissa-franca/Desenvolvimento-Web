423a9249e323ae73660540a3f4b7e2cd46f50b39
larissaRF <larissa_lrf@hotmail.com>
2023-02-15 21:58:39 -0300

    k

--- Lista de Exercicios Funcoes/3.js
+++ Lista de Exercicios Funcoes/3.js
@@ -1,4 +1,4 @@
-// Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+//Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
 
 function conta(a, b) {
     return a ** b
