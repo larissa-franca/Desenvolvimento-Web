bbb86347d29a7173bad005e352133c97331342e4
larissaRF <larissa_lrf@hotmail.com>
2023-02-19 17:02:51 -0300

    free

--- Node/global.js
+++ Node/global.js
@@ -1,7 +1,7 @@
-//console.log(global)
-globalThis.MinhaApp = {
+//console.log(global) o FREEZE é colocado para tornar o objeto imutável
+global.MinhaApp = Object.freeze ({
     saudacao() {
         return 'Hello world!'
     },
     nome: 'Sistema Legal'
-}
+})
