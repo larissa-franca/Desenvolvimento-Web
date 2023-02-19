3b58d1c3d31fd377cab93151a21b12dd18755218
larissaRF <larissa_lrf@hotmail.com>
2023-02-19 17:59:14 -0300

    dir

--- Node/arquivoLeitura.js
+++ Node/arquivoLeitura.js
@@ -14,3 +14,8 @@ fs.readFile(caminho, 'utf-8', (err, conteudo) => {
 
 const config = require('./arquivo.json')
 console.log(config.db)
+
+fs.readdir(__dirname, (err, arquivos) => {
+    console.log('Conteudo da pasta...')
+    console.log(arquivos)
+})
