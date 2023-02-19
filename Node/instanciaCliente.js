fbdc7a95c8fc4c80ffe98027fca28ccd8ab1c980
larissaRF <larissa_lrf@hotmail.com>
2023-02-19 15:53:42 -0300

    cont

--- Node/instanciaCliente.js
+++ Node/instanciaCliente.js
@@ -1,9 +1,14 @@
 const contadorA = require('./instanciaUnica')
 const contadorB = require('./instanciaUnica')
-
+ 
 const contadorC = require('./instanciaNova')()
 const contadorD = require('./instanciaNova')()
-
+ 
+ 
 contadorA.inc()
 contadorA.inc()
-console.log(contadorB.valor)
+console.log(contadorB.valor)// Compartilham o mesmo objeto
+ 
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor)
