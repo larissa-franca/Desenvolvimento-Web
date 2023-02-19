a160cff8ad2225d394132b3e9d3e7176831d8bce
larissaRF <larissa_lrf@hotmail.com>
2023-02-19 17:07:57 -0300

    tis

--- Node/this.js
+++ Node/this.js
@@ -2,3 +2,14 @@ console.log(this === global) // o this não aponta para global e sim para module
 console.log(this === module)
 console.log(this === module.exports)
 console.log(this === exports)
+
+function logThis() {
+    console.log('Dentro de uma função ... ')
+    console.log(this === exports)
+    console.log(this === module.exports)
+    console.log(this === global)
+
+}
+logThis()
+
+//dentro de uma função o this aponta para global
