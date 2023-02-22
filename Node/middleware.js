aa216bff4ed78577587cb18753aec8519386858a
larissaRF <larissa_lrf@hotmail.com>
2023-02-22 20:28:40 -0300

    ctx

--- Node/middleware.js
+++ Node/middleware.js
@@ -4,3 +4,16 @@
 //chain of responsability
 //possui dados de entrada, com sequencia de passo, não tem acoplamento dos proximos passos.
 //Os passos são independentes
+
+//Meddleware pattern (chain of responsability)
+const passo1 = (ctx, next) => {
+    ctx.valor1 = 'mid1'
+    next()
+}
+
+const passo2 = (ctx, next) => {
+    ctx.valor2 = 'mid2'
+    next()
+}
+
+const passo3 = (ctx)
