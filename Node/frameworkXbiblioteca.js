4a693c51b49bb3adb5a280b33e82a9458fc5694b
larissaRF <larissa_lrf@hotmail.com>
2023-02-21 21:07:04 -0300

    anot

---
+++ Node/frameworkXbiblioteca.js
@@ -0,0 +1,7 @@
+//Os frameworks têm um caráter mais completo que as bibliotecas. 
+//Biliotecas são mais pontuais, tendo um escopo de aplicação mais restrito. 
+//É importante notar que o uso de bibliotecas e frameworks podem representar segurança e estabilidade em suas aplicações, 
+//pois a tarefa de montar funcionalidades do zero, além de ser custosa, não garante que boas práticas de segurança e desempenho estejam 
+//em uso. Um exemplo é o Express, um framework HTTP que garante a você estabilidade e tranquilidade futura quando utilizado. 
+//Ao não usar um framework assim, você precisaria de bastante tempo e estudo para desenvolver as suas funcionalidades, 
+//bem como para mantê-las atualizadas.
