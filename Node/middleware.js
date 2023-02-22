12371ccc8f66aa161be999027009e4aa8269c7a6
larissaRF <larissa_lrf@hotmail.com>
2023-02-22 20:18:10 -0300

    COR

---
+++ Node/middleware.js
@@ -0,0 +1,6 @@
+//Processo A
+//Passo A ----> passo B ----> passo C
+
+//chain of responsability
+//possui dados de entrada, com sequencia de passo, não tem acoplamento dos proximos passos.
+//Os passos são independentes
