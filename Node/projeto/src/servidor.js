77513f4c4dd0431c98faa9d604a4051697c1da2c
larissaRF <larissa_lrf@hotmail.com>
2023-02-23 20:53:55 -0300

    proj

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -1,1 +1,7 @@
 const porta = 3003
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'notebook', preco: 123.45 }) //converte para JSON
+})
