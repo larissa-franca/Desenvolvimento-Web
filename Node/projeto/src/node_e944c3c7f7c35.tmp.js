99af485d2de9fb94229a7262644faa939b0f1809
larissaRF <larissa_lrf@hotmail.com>
2023-02-23 20:54:46 -0300

    porta

---
+++ Node/projeto/src/node_e944c3c7f7c35.tmp.js
@@ -0,0 +1,10 @@
+const porta = 3003
+const express = require('express')
+const app = express()
+
+app.get('/produtos', (req, res, next) => {
+    res.send({ nome: 'notebook', preco: 123.45 }) //converte para JSON
+})
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}.`)
+})
