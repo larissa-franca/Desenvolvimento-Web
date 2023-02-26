a3c01246abd18dd5efd978805f39543080317e5c
larissaRF <larissa_lrf@hotmail.com>
2023-02-25 22:53:45 -0300

    N

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'notebook', preco: 123.45 }) //converte para JSON
+    res.send({ nome: 'Notebook', preco: 123.45 }) //converte para JSON
 })
 app.listen(porta, () => {
     console.log(`Servidor executando está na porta: ${porta}.`)
