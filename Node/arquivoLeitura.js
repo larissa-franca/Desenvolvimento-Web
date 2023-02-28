1fdfe771cbacb79f359dd3e525b52b9764ddc877
larissaRF <larissa_lrf@hotmail.com>
2023-02-27 21:43:52 -0300

    A

--- Node/arquivoLeitura.js
+++ Node/arquivoLeitura.js
@@ -6,7 +6,7 @@ const caminho = __dirname + '/arquivo.json'
 const conteudo = fs.readFileSync(caminho, 'utf-8')
 console.log(conteudo)
 
-//assincrono
+//Assincrono
 fs.readFile(caminho, 'utf-8', (err, conteudo) => {
     const config = JSON.parse(conteudo)
     console.log(`${config.db.host}:${config.db.port}`)
