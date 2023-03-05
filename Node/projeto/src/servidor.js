57386dac8ce6bc49c7d74cadb64f7fffbcc244fe
larissaRF <larissa_lrf@hotmail.com>
2023-03-05 19:47:56 -0300

    adad

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,15 +2,14 @@ const porta = 3004
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
-app.get('/produtos', (req, res, next) => {
-    console.log('Middleware 1...')
-    next()
-})
 
 app.get('/produtos', (req, res, next) => {
     res.send({ nome: 'Notebook', preco:123.45}) //Converte para JSON
 })
+
+app.get('/produtos/:id', ())
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
