80d53820dd0a2426535353bf49c931c2430fc7b1
larissaRF <larissa_lrf@hotmail.com>
2023-03-05 20:36:32 -0300

    put

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -23,7 +23,20 @@ app.post('/produtos', (req, res, next) => {
     res.send(produto) //JSON
 })
 
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto) //JSON
+})
 
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
+})
+ 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
 })
