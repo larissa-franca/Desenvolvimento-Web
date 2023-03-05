918c8e9f2d472f6b9b3415bdb519f3c8b0fc09ae
larissaRF <larissa_lrf@hotmail.com>
2023-03-05 20:03:56 -0300

    huyv

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -5,6 +5,8 @@ const app = express()
 const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos()) //Converte para JSON
