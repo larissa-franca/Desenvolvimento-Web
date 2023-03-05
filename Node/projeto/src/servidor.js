f152939c0d6ee16c0442452e065cb31770ad1793
larissaRF <larissa_lrf@hotmail.com>
2023-03-05 19:51:47 -0300

    req

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -9,7 +9,16 @@ app.get('/produtos', (req, res, next) => {
     res.send({ nome: 'Notebook', preco:123.45}) //Converte para JSON
 })
 
-app.get('/produtos/:id', ())
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+})
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
