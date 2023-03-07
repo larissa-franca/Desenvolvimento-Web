72536ef44aa31ba35a5bee6028b5af4ad8804b50
larissaRF <larissa_lrf@hotmail.com>
2023-03-07 20:35:42 -0300

    sadas

--- Node/node_7c9c2fc61c3df.tmp.js
+++ Node/node_7c9c2fc61c3df.tmp.js
@@ -1,5 +1,5 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 8 * * 2', function () {
+const tarefa1 = schedule.scheduleJob('*/5 * 13 * * 2', function () {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
