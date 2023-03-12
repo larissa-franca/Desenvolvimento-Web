9707c85d4d5612ecba7b028d5e005da21d1f767f
larissaRF <larissa_lrf@hotmail.com>
2023-03-12 13:16:30 -0300

    a

---
+++ ES Next/Revisao1.js
@@ -0,0 +1,8 @@
+//let e const = palavras reservadas para determinar variáveis
+//let tem escopo de bloco e o var tem escopo de função ou escopo global
+
+{
+    var a = 2
+    let b = 3
+}
+console.log(a)
