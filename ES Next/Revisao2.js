7a76c5879d22e05653c9f6e2b5fcb024408378df
larissaRF <larissa_lrf@hotmail.com>
2023-03-12 16:53:25 -0300

    this

--- ES Next/Revisao2.js
+++ ES Next/Revisao2.js
@@ -1,3 +1,12 @@
 //Arrow function: tem uma sintaxe mais reduzida e é sempre anonima
 const soma = (a, b) => a + b
 console.log(soma(2, 3))
+
+//Arrow function (this)
+const lexico1 = () => console.log(this === exports)
+//nesse caso a função arrow foi escrita diretamente dentro do módulo ou dentro de um arquivo no node
+//sgnifica que o this sempre irá apontar para o exports ou para o module.exports
+//o this sempre apontará para esse objeto
+const lexico2 = lexico1.bind({})
+lexico1()
+lexico2()
