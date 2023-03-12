72d8510e1d97a30620ddc0842581da44ac8f6cb8
larissaRF <larissa_lrf@hotmail.com>
2023-03-12 20:42:31 -0300

    rev3

--- ES Next/Revisao3.js
+++ ES Next/Revisao3.js
@@ -1,3 +1,4 @@
 //ES8 -> Object.values e Object.entries
 const obj = { a:1, b: 2, c:3 }
-console.log(Object.values(obj))
+console.log(Object.values(obj)) //imprime apenas o valor
+console.log(Object.entries(obj)) // imprime o valor e o objeto correspondente
