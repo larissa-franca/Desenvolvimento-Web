82b3e00127f4586727ae97eeccfa99a582bdee23
larissaRF <larissa_lrf@hotmail.com>
2023-03-12 20:45:50 -0300

    rev3

--- ES Next/Revisao3.js
+++ ES Next/Revisao3.js
@@ -1,4 +1,23 @@
 //ES8 -> Object.values e Object.entries
 const obj = { a:1, b: 2, c:3 }
 console.log(Object.values(obj)) //imprime apenas o valor
-console.log(Object.entries(obj)) // imprime o valor e o objeto correspondente
+console.log(Object.entries(obj)) // imprime o array chave-valor
+
+//Melhorias na notação literal
+const nome = 'Ana'
+const pessoa = {
+    nome,
+    ola() {
+        return 'Oi gente!'
+    }
+}
+console.log(pessoa.nome, pessoa.ola())
+
+//class
+class Animal {}
+class Cachorro extends Animal {
+    falar() {
+        return 'Au au'
+    }
+}
+console.log(new Cachorro().falar())
