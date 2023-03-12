642220d0b52e887e223dd3efc57c74055ecd67e4
larissaRF <larissa_lrf@hotmail.com>
2023-03-12 12:44:20 -0300

    nome

--- Node/entradaESaida.js
+++ Node/entradaESaida.js
@@ -2,17 +2,14 @@ const anonimo = process.argv.indexOf('-a') !== -1
 //console.log(anonimo)
 
 if(anonimo) {
-    process.stdout.write('Fala Anônimo!node entradaESaida.js -a\n')
+    process.stdout.write('Fala Anônimo!\n')
     process.exit()
 } else {
     process.stdout.write('Informe seu nome: ')
     process.stdin.on('data', data => {
-        const os = require('os');
-        const texto = `Olá${os.EOL}meu nome é${os.EOL}Fulano!`
-        console.log(texto)
+        const nome = data.toString().replace('\n', '')
 
-
-        //process.stdout.write(` Fala ${nome} !!\n`)
+        process.stdout.write(`Fala ${nome}\n`)
         process.exit()
     })
 }
