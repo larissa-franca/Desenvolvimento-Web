df332b6533f9f114b97f5984d379209f37b19e51
larissaRF <larissa_lrf@hotmail.com>
2023-03-16 20:59:39 -0300

    set

---
+++ ES Next/set.js
@@ -0,0 +1,13 @@
+//estrutura não indexada, que não aceita repetição
+const time = new Set()
+time.add('Vasco')
+time.add('São Paulo').add('Palmeiras').add('Corintians')
+time.add('Flamengo')
+time.add('Vasco')
+
+console.log(time)
+console.log(time.size)
+console.log(time.has('vasco'))
+console.log(time.has('Vasco'))
+time.delete('Flamengo')
+console.log(time.has('VFlamengo'))
