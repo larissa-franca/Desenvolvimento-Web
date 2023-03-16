9476f20f07426da446386e0edfd49b42ce104de3
larissaRF <larissa_lrf@hotmail.com>
2023-03-15 21:45:39 -0300

    fame

---
+++ ES Next/map.js
@@ -0,0 +1,6 @@
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react'))
