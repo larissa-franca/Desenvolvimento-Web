6dc02ccd6d5c11b50eb1b6983af0db72761a19d5
larissaRF <larissa_lrf@hotmail.com>
2023-03-17 23:15:43 -0300

    s

--- ES Next/forOf.js
+++ ES Next/forOf.js
@@ -2,7 +2,7 @@ for (let letra of "Cod3r") {
     console.log(letra)
 }
 
- const assuntosEcma = ['Map', 'Set', 'Promisse']
+ const assuntosEcma = ['Map', 'Set', 'Promise']
 
  for (let i in assuntosEcma) {
     console.log(i)
