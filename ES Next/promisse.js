cf62707dc8a06b92de17b763395b0172d3316d33
larissaRF <larissa_lrf@hotmail.com>
2023-03-18 18:29:49 -0300

    por

--- ES Next/promisse.js
+++ ES Next/promisse.js
@@ -1,2 +1,9 @@
 //estrutura que tem por objetivo representar uma operação assincrona, pode ser atendida ou rejeitada
 
+function falarDepoisDe(segundos, frase) {
+    return new Promisse((resolve, reject) => {
+        setTimeout (() => {
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
