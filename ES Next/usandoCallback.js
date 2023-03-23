c8ea58217d8117e1c2ff52c20cd53aed57c1a403
larissaRF <larissa_lrf@hotmail.com>
2023-03-22 21:01:43 -0300

    cb

--- ES Next/usandoCallback.js
+++ ES Next/usandoCallback.js
@@ -1,1 +1,17 @@
 //Sem promisse
+const http = require('http')
+
+const getTurma = (letra, callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    http.get(url, res => {
+        let resultado = ''
+
+        rmSync.on('data', dados => {
+            resultado += dados
+        })
+
+        res.on('end', () => {
+            callback(JSON.parse(resultados))
+        })
+    })
+}
