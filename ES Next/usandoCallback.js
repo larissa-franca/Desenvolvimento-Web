6632699b2d883fd08786d275e613fade874d517c
larissaRF <larissa_lrf@hotmail.com>
2023-03-22 21:05:41 -0300

    cb

--- ES Next/usandoCallback.js
+++ ES Next/usandoCallback.js
@@ -6,12 +6,12 @@ const getTurma = (letra, callback) => {
     http.get(url, res => {
         let resultado = ''
 
-        rmSync.on('data', dados => {
+        res.on('data', dados => {
             resultado += dados
         })
 
         res.on('end', () => {
-            callback(JSON.parse(resultados))
+            callback(JSON.parse(resultado))
         })
     })
 }
