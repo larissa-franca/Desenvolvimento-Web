ca31cce494d64dd4597a9e70c2a4cc6893ac1528
larissaRF <larissa_lrf@hotmail.com>
2023-03-23 21:35:00 -0300

    .

--- ES Next/taggedTemplate.js
+++ ES Next/taggedTemplate.js
@@ -1,4 +1,4 @@
-//Tagged templates - processa o template dentro de uma função
+//Tagged templates - processa o template dentro de uma função.
 
 function tag(partes, ...valores) {
     console.log(partes)
