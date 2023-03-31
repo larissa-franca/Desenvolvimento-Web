c06877a2b91b90570b5f9a23a26694d39cf86be1
larissaRF <larissa_lrf@hotmail.com>
2023-03-30 21:10:42 -0300

    e

--- Funções/paramPadroes.js
+++ Funções/paramPadroes.js
@@ -7,7 +7,7 @@ function soma1(a, b, c) {
 }
 console.log(soma1(), soma1(3), soma1(1, 2, 3), soma1(0, 0, 0))
 
-//estrategia 2, 3 e 4 para gerar valor padrão
+//Estrategia 2, 3 e 4 para gerar valor padrão
 function soma2(a, b, c) {
     a = a !== undefined ? a : 1
     b = 1 in arguments ? b : 1
