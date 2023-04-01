a537393f65cf6cd1a4dcadc00224e34d8eff5b23
larissaRF <larissa_lrf@hotmail.com>
2023-03-31 21:26:17 -0300

    e

--- Funções/paramPadroes.js
+++ Funções/paramPadroes.js
@@ -17,7 +17,7 @@ function soma2(a, b, c) {
 console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
 
 
-//Valor padrão do es2015
+//Valor padrão do ES 2015
 
 function soma3(a = 1, b = 1, c = 1) {
     return a + b + c
