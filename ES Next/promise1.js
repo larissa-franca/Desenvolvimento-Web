b7b5f279634d875585a1b5d22b6acc31ddd24f69
larissaRF <larissa_lrf@hotmail.com>
2023-04-06 21:09:52 -0300

    dfgdf

--- ES Next/promise1.js
+++ ES Next/promise1.js
@@ -3,7 +3,7 @@ let p = new Promise(function(cumprirPromessa){
    //Uma promessa de algo que vai ser executado no futuro, e um dado que vai ser devolvido no futuro
    })
    //para conseguir gerar esse dado, é necessário passar uma função para a promise
-   //a função passada para a promise vai ser chamada quando se quiser cumprir a promessa
+   //a função passada para a promise vai ser chamada quando se quiser cumprir a promessa...
     
    p.then(function(valor){ //O then também recebe uma função como parâmetro
        console.log(valor) //O valor passado para a função do then é aquele que foi cumprido pela função da promise,  
