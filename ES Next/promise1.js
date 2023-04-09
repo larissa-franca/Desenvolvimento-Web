eb3290b4c142ad5a1f53e2fb6405b30ce447f0e9
larissaRF <larissa_lrf@hotmail.com>
2023-04-08 22:38:18 -0300

    '

--- ES Next/promise1.js
+++ ES Next/promise1.js
@@ -5,6 +5,6 @@ let p = new Promise(function(cumprirPromessa){
    //Para conseguir gerar esse dado, é necessário passar uma função para a promise...
    //A função passada para a promise vai ser chamada quando se quiser cumprir a promessa...
     
-   p.then(function(valor){ //O then também recebe uma função como parâmetro
+   p.then(function(valor){ //O 'then' também recebe uma função como parâmetro
        console.log(valor) //O valor passado para a função do then é aquele que foi cumprido pela função da promise,  
    }) //Também usa o conceito de callback, pois passa uma função como parâmetro e essa função é chamda quando a promessa é cumprida.
