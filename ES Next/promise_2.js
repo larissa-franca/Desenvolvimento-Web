b90d4568cc4c223fb9467934e23e96a6dd8185a7
larissaRF <larissa_lrf@hotmail.com>
2023-04-13 21:19:35 -0300

    .

--- ES Next/promise_2.js
+++ ES Next/promise_2.js
@@ -5,7 +5,7 @@
 //         console.log('Executando callback...')
 
 //         setTimeout(function() {
-//             console.log('Executando callback....')
+//             console.log('Executando callback...')
 
 //         }, 2000)
 //     }, 2000)
