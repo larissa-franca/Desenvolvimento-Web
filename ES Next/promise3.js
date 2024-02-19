732a2d2e69d58a823f279c5fc47f427bcfcd2ebf
larissaRF <larissa_lrf@hotmail.com>
2024-02-19 15:45:32 -0300

    promisse

--- ES Next/promise3.js
+++ ES Next/promise3.js
@@ -1,9 +1,13 @@
+const { parseInt } = require("lodash")
+
 function gerarNumerosEntre(min, max) {
     if(min > max) {
         [max, min] = [min, max]
     }
     return new Promise(resolve => {
-        resolve(7)
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        resolve(aleatorio)
     })
 }
 gerarNumerosEntre(10, 30).then(console.log)
