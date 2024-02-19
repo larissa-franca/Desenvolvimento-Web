e36bb151b0ab46b47dc057bdbd3cbd6a3e316991
larissaRF <larissa_lrf@hotmail.com>
2024-02-19 16:47:29 -0300

    promise

---
+++ ES Next/promise+5.js
@@ -0,0 +1,15 @@
+const { reject } = require("lodash")
+
+function funcionarOuNao(valor, chanceErro) {
+    return new Promise((resolve, reject) => {
+        if(Math.random() < chanceErro) {
+            reject('Ocorreu um erro!')
+        } else {
+            resolve(valor)
+        }
+    })
+}
+
+funcionarOuNao('Testando...', 0.9)
+    .then(v => console.log(``))
+    .catch(err => console.log(`Erro: ${err}`))
